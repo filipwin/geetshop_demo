67b99dccdd71c1b0763363a98a98bfe0f99fb2d7
filipwin <winiarzf@gmail.com>
2022-12-11 22:45:15 +0100

    GetView demo

--- lib/ui/product/screens/product_list_screen.dart
+++ lib/ui/product/screens/product_list_screen.dart
@@ -3,15 +3,14 @@ import 'package:get/get.dart';
 import 'package:getshop_demo/controllers/product_list_controller.dart';
 import 'package:getshop_demo/ui/product/components/product_list.dart';
 
-class ProductListScreen extends StatelessWidget {
+class ProductListScreen extends GetView<ProductListController> {
   const ProductListScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final productListController = Get.find<ProductListController>();
     return Container(
       color: Colors.white,
-      child: productListController.obx(
+      child: controller.obx(
         (state) => ProductList(products: state ?? []),
         onError: (e) => const Center(
           child: Text('Failed loading product list'),
