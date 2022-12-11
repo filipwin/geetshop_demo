3c957a2b5186bb97aeae37a9c790e327c9cf0a14
filipwin <winiarzf@gmail.com>
2022-12-11 22:45:04 +0100

    logging using worker

--- lib/controllers/cart_controller.dart
+++ lib/controllers/cart_controller.dart
@@ -6,6 +6,12 @@ class CartController extends GetxController {
   final _cart = <Product>[].obs;
   RxList<Product> get cart => _cart;
 
+  @override
+  void onInit() {
+    ever<List<Product>>(cart, (rx) => print("Number of items in cart: ${rx.length}"));
+    super.onInit();
+  }
+
   void addToCart(Product product) => _cart.add(product);
   void removeFromCart(Product product) => _cart.remove(product);
 }
